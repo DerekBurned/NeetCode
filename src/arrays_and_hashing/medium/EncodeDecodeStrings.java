61add86c7f39014cb7dd569553a52ff0af76a1d3
Derek <danillolka2@gmail.com>
2026-03-11 00:21:02 +0100

    Init commit, TopKFrequentElements.java genius

---
+++ src/arrays_and_hashing/medium/EncodeDecodeStrings.java
@@ -0,0 +1,43 @@
+package arrays_and_hashing.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EncodeDecodeStrings {
+    public static String encode(List<String> strs) {
+        for(String str : strs){
+            switch (str.length() % 3){
+                case 0:
+
+                    break;
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                default:
+
+                    break;
+            }
+        }
+        return String.join(" ", strs);
+    }
+
+    public static List<String> decode(String str) {
+
+        return new ArrayList<>();
+    }
+    public static String encodemethod1(String str){
+        
+        return "";
+    }
+    public static String encodemethod2(String str){
+
+        return "";
+    }
+    public static String encodemethod3(String str){
+
+        return "";
+    }
+}
--- src/arrays_and_hashing/medium/TopKFrequentElements.java
+++ src/arrays_and_hashing/medium/TopKFrequentElements.java
@@ -1,23 +1,24 @@
 package arrays_and_hashing.medium;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class TopKFrequentElements {
 
     public static void main(String[] args) {
-
+        int[] nums ={1,1,1,1,1,2,2,3,3,3};
+        int k = 2;
+        System.out.println(Arrays.toString(topKFrequent(nums, k)));
     }
-    public int[] topKFrequent(int[] nums, int k) {
+    public static int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> freq = new HashMap<>();
         for (int n : nums) {
             freq.put(n, freq.getOrDefault(n, 0) + 1);
         }
-
-
-
-        return new int[0];
+        return freq.entrySet().stream()
+                .sorted((a, b) -> b.getValue() - a.getValue())
+                .limit(k)
+                .mapToInt(Map.Entry::getKey)
+                .toArray();
     }
 }
 
