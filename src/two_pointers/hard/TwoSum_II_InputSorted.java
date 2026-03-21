c7fdd8ca9b6813e0f681ae83602d050d7046002a
Derek <danillolka2@gmail.com>
2026-03-21 16:38:52 +0100

    TwoSum_II_InputSorted same as TwoSum success

---
+++ src/two_pointers/hard/TwoSum_II_InputSorted.java
@@ -0,0 +1,41 @@
+package two_pointers.hard;
+
+import java.util.HashMap;
+
+public class TwoSum_II_InputSorted {
+
+        public static void main(String[] args) {
+            int[] nums = {2, 7, 11, 15};
+            int target = 9;
+            int[] result = twoSumTwoPointers(nums, target);
+            System.out.println("[" + result[0] + ", " + result[1] + "]");
+        }
+
+        public static int[] twoSum(int[] nums, int target) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int complement = target - nums[i];
+                if (map.containsKey(complement)) {
+                    return new int[]{map.get(complement)+1, i+1};
+                }
+                map.put(nums[i], i);
+            }
+            return new int[]{};
+        }
+
+    public static int[] twoSumTwoPointers(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+}
